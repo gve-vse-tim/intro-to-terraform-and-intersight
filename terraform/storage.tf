630f4ee5f28bb881177982c9d726e090c8a0d0b3
Tim Miller <timmil@cisco.com>
2022-04-25 10:31:57 -0400

    Update storage policies to match changes in provider resources.

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -1,82 +1,15 @@
-# Define the Disk Group policy
-resource "intersight_storage_disk_group_policy" "RAID6-Disk-Group" {
+# Define the Storage Policy
+resource "intersight_storage_storage_policy" "standalone_storage" {
     organization {
         moid = intersight_organization_organization.target.moid
     }
 
     description = "Terraform deployed"
-    name = "RAID6-Disk-Group"
-    raid_level = "Raid6"
-    use_jbods = true
+    name = "Standalone Server Storage Policy"
+    use_jbod_for_vd_creation = true
 
-    span_groups {
-        disks {
-            slot_number = 1
-        }
-        disks {
-            slot_number = 2
-        }
-        disks {
-            slot_number = 3
-        }
-        disks {
-            slot_number = 4
-        }
-        disks {
-            slot_number = 5
-        }
-        disks {
-            slot_number = 6
-        }
-        disks {
-            slot_number = 7
-        }
-        disks {
-            slot_number = 8
-        }
-        disks {
-            slot_number = 9
-        }
-        disks {
-            slot_number = 10
-        }
-        disks {
-            slot_number = 11
-        }
-        disks {
-            slot_number = 12
-        }
-    }
-}
-
-output "storage_disk_group_moid" {
-    value = intersight_storage_disk_group_policy.RAID6-Disk-Group.moid
-}
-
-# Define the storage policy
-resource "intersight_storage_storage_policy" "server1-Storage" {
-    organization {
-        moid = intersight_organization_organization.target.moid
-    }
-
-    description = "Terraform deployed"
-    name = "server1-Storage"
-    retain_policy_virtual_drives = true
     unused_disks_state = "UnconfiguredGood"
-
-    virtual_drives {
-        name = "LOCAL_RAID6"
-        disk_group_policy = intersight_storage_disk_group_policy.RAID6-Disk-Group.moid
-        size = 0
-        expand_to_available = true
-        boot_drive = true
-        access_policy = "ReadWrite"
-        drive_cache = "Enable"
-        io_policy = "Default"
-        strip_size = "Default"
-        read_policy = "Default"
-        write_policy = "Default"
-    }
+    global_hot_spares = 0
 
     profiles {
         object_type = "server.Profile"
@@ -85,5 +18,41 @@ resource "intersight_storage_storage_policy" "server1-Storage" {
 }
 
 output "storage_policy_moid" {
-    value = intersight_storage_storage_policy.server1-Storage.moid
+    value = intersight_storage_storage_policy.standalone_storage.moid
+}
+
+# Define the Storage Drive Group
+resource "intersight_storage_drive_group" "single_raid6_volume" {
+    name = "Single Volume, 12-slot RAID6 "
+    raid_level = "Raid6"
+
+    manual_drive_group {
+        span_groups {
+            slots = "1,2,3,4,5,6,7,8,9,10,11,12"
+        }
+    }
+
+    virtual_drives {
+        name = "LOCAL_RAID6"
+        size = 0
+        expand_to_available = true
+        boot_drive = true
+
+        virtual_drive_policy {
+            access_policy = "ReadWrite"
+            drive_cache = "Enable"
+            strip_size = 64
+            read_policy = "Default"
+            write_policy = "Default"
+        }
+    }
+
+    # Map this configuration to the following storage policies
+    storage_policy {
+        moid = intersight_storage_storage_policy.standalone_storage.moid
+    }
+}
+
+output "storage_disk_group_moid" {
+    value = intersight_storage_drive_group.single_raid6_volume.moid
 }
